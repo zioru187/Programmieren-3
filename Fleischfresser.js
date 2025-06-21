56873eebee68928635f05b2e9e59dbb8c0130be8
zioru187 <zioru187@tumo.world>
2025-06-21 14:59:08 +0200

    Erster commit

---
+++ Fleischfresser.js
@@ -0,0 +1,95 @@
+class FleischFresser {
+    zeile;
+    spalte;
+    energie = 20;
+    constructor(z,s) {
+        this.zeile = z;
+        this.spalte = s;
+        this.platziereSelbstInMatrix();
+    };
+    platziereSelbstInMatrix() {
+        matrix[this.zeile][this.spalte] = 3;
+    };
+    spielzug() {
+        if (this.energie > 20) {
+            this.platziereNeuenFleischFresser();
+            this.energie = 20;
+        } else if (this.energie > 0 ) {
+            this.machSchritt();
+        } else {
+            matrix[this.zeile][this.spalte] = 0;
+            löschObjekt(FleischfresserArray,this.zeile,this.spalte);
+        }
+    };
+    platziereNeuenFleischFresser() {
+        // scan deine umgebung, und schau ob du um dich herum 
+        // ein freies Feld findest.
+        // wenn du ein freies feld findest, dann platziere auf diesem Feld
+        // ein neues Grasobjekt. 
+        // 0 -> OBEN
+        // 1 -> RECHTS
+        // 2 -> LINKS
+        // 3 -> UNTEN
+        let richtung = randomNumber(0, 4);
+        let benachbarteFelder = [
+            [this.zeile - 1, this.spalte],
+            [this.zeile, this.spalte - 1],
+            [this.zeile + 1, this.spalte],
+            [this.zeile, this.spalte + 1],
+        ]
+        let ausgewähltesFeld = benachbarteFelder[richtung];
+        // HIER KOMMT CODE REIN DER ÜBERPRÜFT
+        // OB DIE KOORDINATEN INNERHALB DER MATRIX LIEGEN
+        if (inMatrix(ausgewähltesFeld)) {
+            console.log("yay, feld ist in matrix")
+        } else {
+            return;
+        }
+
+        for (let i = 0; i < 4; i++) {
+            let j = (richtung + i) % 4;
+            let ausgewähltesFeld = benachbarteFelder[j];
+            if (inMatrix(ausgewähltesFeld)) {
+                if (scanFeld(ausgewähltesFeld,1)) {
+                    let zeile = ausgewähltesFeld[0];
+                    let spalte = ausgewähltesFeld[1];
+
+                    FleischfresserArray.push(new FleischFresser(zeile, spalte))
+                    return;
+                }
+            }
+
+        }
+
+    }
+    machSchritt() {
+        // 0 -> OBEN
+        // 1 -> RECHTS
+        // 2 -> LINKS
+        // 3 -> UNTEN
+        let richtung = randomNumber(0, 4);
+        let benachbarteFelder = [
+            [this.zeile - 1, this.spalte],
+            [this.zeile, this.spalte - 1],
+            [this.zeile + 1, this.spalte],
+            [this.zeile, this.spalte + 1],
+        ]
+        for (let i = 0; i < 4; i++) {
+            let j = (richtung + i) % 4
+            let ausgewähltesFeld = benachbarteFelder[j];
+            if (inMatrix(ausgewähltesFeld)) {
+                if (scanFeld(ausgewähltesFeld,1)) {
+                    matrix[this.zeile][this.spalte] = 0;
+                    löschObjekt(RasenDestroyerArray,ausgewähltesFeld[0],ausgewähltesFeld[1])
+                    this.zeile = ausgewähltesFeld[0];
+                    this.spalte = ausgewähltesFeld[1];
+                    matrix[this.zeile][this.spalte] = 3;
+                    this.energie++;
+                    return;
+                }
+            }
+
+        }
+        this.energie--;
+    };
+}
---
+++ script.js
@@ -0,0 +1,55 @@
+let el1 = document.getElementById("element1");
+
+let matrix = erstelleMatrix(50,50);
+let grasArray = []
+let RasenDestroyerArray = [] 
+let FleischfresserArray = []
+
+function setup() {
+  createCanvas(500, 500);
+  frameRate(10)
+  noStroke()
+  for (let i = 0; i < 1; i++) {
+   
+    grasArray.push(new gras(23,21))
+    grasArray.push(new gras(25,22))
+    grasArray.push(new gras(25,23))
+    grasArray.push(new gras(25,24))
+    grasArray.push(new gras(25,25))
+    grasArray.push(new gras(21,25))
+    grasArray.push(new gras(22,25))
+    grasArray.push(new gras(23,25))
+    grasArray.push(new gras(24,25))
+  }
+  for (let i = 0; i < 1; i++) {
+    RasenDestroyerArray.push(new RasenDestroyer(22,22))
+  }
+  //for (let i = 0; i < 1; i++) {
+  //  FleischfresserArray.push(new RasenDestroyer(22,22))
+  //}
+}
+
+let z = 0
+function draw() {
+  console.log(grasArray.length+RasenDestroyerArray.length);
+  //console.log(RasenDestroyerArray)
+  //console.log(grasArray)
+  if (z < 300) {
+    for (let i = 0; i < grasArray.length; i++) {
+      grasArray[i].spielzug()
+    }
+    for (let i = 0; i < RasenDestroyerArray.length; i++) {
+      RasenDestroyerArray[i].spielzug()
+    }
+    for (let i = 0; i < FleischfresserArray.length; i++) {
+      FleischfresserArray[i].spielzug()
+    }
+    zeichneMatrix(3, 3);
+  }
+  z++;
+}
+
+
+
+
+
